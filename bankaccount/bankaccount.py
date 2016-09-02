95597f705b847c24d7ce303082ea8e21adaaa88b
Adam Szewczyk <a-szewczyk@pdc.proto-g.co.jp>
2016-09-02 15:12:40 +0900

    Added sample class

---
+++ bankaccount/bankaccount.py
@@ -0,0 +1,30 @@
+class BankAccount:
+    """
+        Simple class representing a bank account supporting deposit,
+        withdrawal, and statement generation.
+    """
+    def __init__(self, starting_balance=0):
+        """
+           Creates a new Monitor object with no configuration
+        """
+        self.balance = starting_balance
+
+    def deposit(self, amount):
+        """
+           Adds money to the account.
+        """
+        self.balance += amount
+
+    def withdraw(self, amount):
+        self.balance -= amount
+
+    def display_balance(self):
+        print ("Balance ", self.balance)
+
+if __name__ == '__main__':
+    account = BankAccount(100)
+    account.deposit(20)
+    account.withdraw(40)
+    account.display_balance()
+    
+    
